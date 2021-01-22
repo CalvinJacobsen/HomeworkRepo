3303f24acdd60c995667f7971b9ca8e4ebc61654
Calvin Jacobsen <Jaco1932@umn.edu>
2021-01-21 23:09:55 -0600

    added time tracking mechanism to website

--- Homework5/Assets/script.js
+++ Homework5/Assets/script.js
@@ -1,14 +1,43 @@
 var $currentDay = $("currentDay");
-var $timeBlocks = $(".time-block");
+var $timeRows = $(".time-block");
 var $scheduleArea = $(".schedule");
+var $textbox = $(".textbox");
 
 var toDoItems = [];
 var currentHour = moment().format("H");
 var currentDate = moment().format("dddd, MMMM Do");
 
-function initializeSchedule(){
+function initializeSchedule() {
+
+
+
+
 
 
 }
 
-initializeSchedule()
+function renderColorTime() {
+
+    $textbox.each(function () {
+        //get time id for each row
+        var blockHour = parseInt($(this).attr("hour-id"));
+        console.log(blockHour);
+
+        //sets appropriate colors for each cell
+        if (blockHour == currentHour) {
+            $(this).addClass("present").removeClass("past future");
+        }
+        if (blockHour < currentHour) {
+            $(this).addClass("past").removeClass("present future");
+        }
+        if (blockHour > currentHour){
+            $(this).addClass("future").removeClass("past present");
+        }
+    
+    });
+};
+
+
+
+renderColorTime();
+initializeSchedule();
