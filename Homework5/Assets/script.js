59be35e04c9b6bc2264458e54fd36face56d6392
Calvin Jacobsen <Jaco1932@umn.edu>
2021-01-21 23:46:24 -0600

    added object array for items within the text

--- Homework5/Assets/script.js
+++ Homework5/Assets/script.js
@@ -1,21 +1,17 @@
-var $currentDay = $("currentDay");
+var $currentDay = $(".currentDay");
 var $timeRows = $(".time-block");
 var $scheduleArea = $(".schedule");
 var $textbox = $(".textbox");
 
-var toDoItems = [];
+//i looked this one up, for moment 
 var currentHour = moment().format("H");
 var currentDate = moment().format("dddd, MMMM Do");
 
-function initializeSchedule() {
+//updating current date onto page
+console.log(currentDate);
+$(".currentDay").append(currentDate);
 
-
-
-
-
-
-}
-
+//function for rendering te time
 function renderColorTime() {
 
     $textbox.each(function () {
@@ -25,19 +21,57 @@ function renderColorTime() {
 
         //sets appropriate colors for each cell
         if (blockHour == currentHour) {
-            $(this).addClass("present").removeClass("past future");
+            $(this).addClass("present")
+            $(this).removeClass("past future");
         }
         if (blockHour < currentHour) {
-            $(this).addClass("past").removeClass("present future");
+            $(this).addClass("past")
+            $(this).removeClass("present future");
         }
-        if (blockHour > currentHour){
-            $(this).addClass("future").removeClass("past present");
+        if (blockHour > currentHour) {
+            $(this).addClass("future")
+            $(this).removeClass("past present");
         }
     
     });
 };
 
+//creating object
+toDoObjArray = [];
+
+//putting each object that we have so far into our object
+function objectSchedule() {
+    
+    $textbox.each(function () {
+        var blockHour = parseInt($(this).attr("hour-id"));
+
+        var toDoObjects = {
+            time: blockHour,
+            text: ""
+        }
+
+        //this function puts our objects into the array we made
+        toDoObjArray.push(toDoObjects);
+    }); 
+    
+}
+
+
+//saves the data locally
+function localStorage(){
+
+    //see which item we need to update based on the hour of the button clicked matching
+    for (var j = 0; j < toDoObjArray.length; j++){
+      if (toDoObjArray[j].hour == hourToUpdate){
+        //set its text to what was added to textarea
+        toDoObjArray[j].text = itemToAdd;
+      }
+    }
+    localStorage.setItem("todos", JSON.stringify(toDoObjArray));
+    renderSchedule();
+  }
 
 
 renderColorTime();
-initializeSchedule();
+objectSchedule();
+$schedule.on("click", "button", localStorage)
